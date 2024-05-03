ae5cc334c63ed4ce6bd27bb480e55546c517e038
Khamdany <30920568+khamdany@users.noreply.github.com>
2024-05-03 22:59:12 +0800

    modified:   scripts/executable_bat.sh

--- scripts/executable_bat.sh
+++ scripts/executable_bat.sh
@@ -5,13 +5,20 @@ bat1=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | rg -o 'percentag
 
 average=$(echo "($bat0 + $bat1) / 2" | bc)
 
+state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -oP 'state:\s+\K\w+')
+
 # echo $average
 # Check if below 20%
 if (( $(echo "$average < 20" | bc -l) )); then
     dunstify Battery 'below 20%' 
-   # echo "0 blink" | sudo tee /proc/acpi/ibm/led 1> /dev/null
-   # echo "10 blink" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+    if [[ $state == "charging" ]]; then
+       echo "0 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+       echo "10 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+    else
+      echo "0 blink" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+      echo "10 blink" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+    fi
  else 
-   # echo "0 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
-   # echo "10 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+   echo "0 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
+   echo "10 on" | sudo tee /proc/acpi/ibm/led 1> /dev/null
 fi
