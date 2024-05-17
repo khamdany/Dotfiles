03a96350293b3d9a151c88936b9db1ccab52ee95
Khamdany <30920568+khamdany@users.noreply.github.com>
2024-05-17 09:57:43 +0800

    Changes to be committed: 	modified:   dot_config/sway/config 	modified:   dot_config/waybar/config 	modified:   scripts/executable_htop.sh

--- scripts/executable_htop.sh
+++ scripts/executable_htop.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# Use command substitution to get the process ID of iwgtk
-pgrep_result=$(pgrep htop)
+# Function to find kitty PID with child htop process
+function find_kitty_htop_pid() {
+  kitty_pids=$(pgrep kitty)
+  for kitty_pid in $kitty_pids; do
+    # Check if kitty has child htop using ps
+    if ps --ppid "$kitty_pid" | grep -q htop; then
+      echo "$kitty_pid"  # return the first kitty PID with child htop
+      return 0  # Exit the function with success (0)
+    fi
+  done
+  return 1  # Exit the function with failure (1)
+}
 
-if [ -z "$pgrep_result" ]; then
-    # If iwgtk is not running, start it
-    exec htop 
+# Find kitty with htop (check all instances)
+launchedKittyPid=$(find_kitty_htop_pid)
+if [[ $? -eq 0 ]]; then
+  echo "Launched kitty with htop (PID: $launchedKittyPid)"
 else
-    # If iwgtk is already running, kill it
-    kill "$pgrep_result"
+  echo "No kitty process found with htop. Launching kitty with htop..."
+  kitty htop &  # Launch kitty with htop in the background
+  # Re-run find_kitty_htop_pid to get the newly launched process PID
+  launchedKittyPid=$(find_kitty_htop_pid)
+  if [[ $? -eq 0 ]]; then
+    echo "Successfully launched kitty with htop (PID: $launchedKittyPid)"
+  else
+    echo "Failed to launch kitty with htop process"
+    exit 1  # Exit with error code if launch failed
+  fi
 fi
+
+# Kill kitty process when script exits
+kill "$launchedKittyPid"
+echo "Killed kitty (PID: $launchedKittyPid)"
